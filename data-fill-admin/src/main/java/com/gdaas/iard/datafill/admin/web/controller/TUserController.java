b85a062c7a2e5b45e55fe34dd8ec97be5e386adc
y736674690 <736674690@qq.com>
2019-10-05 16:42:18 +0800

    Revert "于浩洋"
    This reverts commit 9c2c9dd366fdc2468d11a9fa12803c3bdd82e398.

--- data-fill-admin/src/main/java/com/gdaas/iard/datafill/admin/web/controller/TUserController.java
+++ data-fill-admin/src/main/java/com/gdaas/iard/datafill/admin/web/controller/TUserController.java
@@ -6,64 +6,35 @@
 
 package com.gdaas.iard.datafill.admin.web.controller;
 
-import com.gdaas.iard.datafill.admin.service.TUserService;
+import com.alibaba.fastjson.JSONObject;
 import com.gdaas.iard.datafill.admin.repo.dao.entity.TUserEntity;
-import com.gdaas.iard.datafill.admin.web.common.BaseResp;
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.gdaas.iard.datafill.admin.service.TUserService;
+import com.gdaas.iard.datafill.common.BaseRequest;
+import com.gdaas.iard.datafill.common.BaseResp;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-import java.util.List;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
 
 /**
- * <p>用户表 前端控制器</p>
+ * <p> 前端控制器</p>
  *
  * 增POST 删DELETE 改PUT 查GET
  * CRUD (POST GET PUT DELETE)
  *
  * @author like
- * @since 2019-10-05
+ * @since 2019-09-24
  */
+@Log4j2
 @Api(tags = "TUserController")
-@RestController
-@RequestMapping("/TUserController")
+@RestController("TUserController")
+@RequestMapping("/user")
 public class TUserController {
     @Autowired
     private TUserService targetService;
-
-    /**
-     * 获取数据列表
-     *
-     * @author jerryniu
-     */
-    @ApiOperation("查询分页")
-    @GetMapping("/list")
-    public BaseResp findListByPage(@RequestParam(name = "page", defaultValue = "1") int pageIndex,
-            @RequestParam(name = "rows", defaultValue = "20") int step) {
-        Page page = new Page(pageIndex, step);
-        targetService.page(page, null);
-        return BaseResp.success(page);
-    }
-
-    /**
-     * 获取全部数据
-     *
-     * @author jerryniu
-     */
-    @ApiOperation("查询所有数据")
-    @GetMapping("/all")
-    public BaseResp findAll() {
-        List<TUserEntity> models = targetService.list(null);
-        return BaseResp.success(models);
-    }
 
     /**
      * 根据ID查找数据
@@ -72,7 +43,7 @@ public class TUserController {
      */
     @ApiOperation("查询单条记录")
     @GetMapping("/find")
-    public BaseResp find(String id) {
+    public BaseResp find(Long id) {
         TUserEntity entity = targetService.getById(id);
         if (entity == null) {
             return BaseResp.fail("尚未查询到此ID");
@@ -80,48 +51,6 @@ public class TUserController {
         return BaseResp.success(entity);
     }
 
-    /**
-     * 添加数据
-     *
-     * @author jerryniu
-     */
-    @ApiOperation(value = "添加单条记录", notes = "id自增")
-    @PostMapping(value = "/add")
-    public BaseResp addItem(@RequestBody TUserEntity entity) {
-        boolean isOk = targetService.save(entity);
-        if (isOk) {
-            return BaseResp.success("数据添加成功");
-        }
-        return BaseResp.fail("数据添加失败");
-    }
+ 
 
-    /**
-     * 更新数据
-     *
-     * @author jerryniu
-     */
-    @ApiOperation("更新单条记录")
-    @PutMapping(value = "/update")
-    public BaseResp updateItem(@RequestBody TUserEntity entity) {
-        boolean isOk = targetService.updateById(entity);
-        if (isOk) {
-            return BaseResp.success("数据更改成功");
-        }
-        return BaseResp.fail("数据更改失败");
-    }
-
-    /**
-     * 删除数据
-     *
-     * @author jerryniu
-     */
-    @ApiOperation("删除记录")
-    @DeleteMapping("/del")
-    public BaseResp deleteItems(List<String> ids) {
-        boolean isOk = targetService.removeByIds(ids);
-        if (isOk) {
-            return BaseResp.success("数据删除成功");
-        }
-        return BaseResp.fail("数据删除失败");
-    }
 }
